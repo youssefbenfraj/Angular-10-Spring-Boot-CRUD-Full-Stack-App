3fe240f5f1dc10cde57842d3d9a8e669cee4e5e8
Youssef Ben Fraj <93080017+youssefbenfraj@users.noreply.github.com>
2023-07-12 13:58:40 +0100

    Update employee.service.ts

--- angular-frontend/src/app/employee.service.ts
+++ angular-frontend/src/app/employee.service.ts
@@ -8,7 +8,7 @@ import { Employee } from './employee';
 })
 export class EmployeeService {
 
-  private baseURL = "http://Emppspring:8080/api/v1/employees";
+  private baseURL = "http://emppspring:8080/api/v1/employees";
 
   constructor(private httpClient: HttpClient) { }
   
