d36902745318de4a559dea7e802a384e85480261
Youssef Ben Fraj <93080017+youssefbenfraj@users.noreply.github.com>
2023-07-16 12:51:23 +0100

    Update EmployeeController.java

--- springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -20,7 +20,7 @@ import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "angular-service")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
