99cf82144be375a691da08cd33324fe907448b78
Youssef Ben Fraj <93080017+youssefbenfraj@users.noreply.github.com>
2023-07-18 13:56:38 +0100

    Update employee.service.ts

--- angular-frontend/src/app/employee.service.ts
+++ angular-frontend/src/app/employee.service.ts
@@ -8,27 +8,27 @@ import { Employee } from './employee';
 })
 export class EmployeeService {
 
-  private baseURL = "springservice.com/api/v1/employees";
+  private baseURL = "/api/v1/employees";
 
   constructor(private httpClient: HttpClient) { }
   
   getEmployeesList(): Observable<Employee[]>{
-    return this.httpClient.get<Employee[]>(`http://`+`${this.baseURL}`);
+    return this.httpClient.get<Employee[]>(`${this.baseURL}`);
   }
 
   createEmployee(employee: Employee): Observable<Object>{
-    return this.httpClient.post(`http://`+`${this.baseURL}`, employee);
+    return this.httpClient.post(`${this.baseURL}`, employee);
   }
 
   getEmployeeById(id: number): Observable<Employee>{
-    return this.httpClient.get<Employee>(`http://`+`${this.baseURL}/${id}`);
+    return this.httpClient.get<Employee>(`${this.baseURL}/${id}`);
   }
 
   updateEmployee(id: number, employee: Employee): Observable<Object>{
-    return this.httpClient.put(`http://`+`${this.baseURL}/${id}`, employee);
+    return this.httpClient.put(`${this.baseURL}/${id}`, employee);
   }
 
   deleteEmployee(id: number): Observable<Object>{
-    return this.httpClient.delete(`http://`+`${this.baseURL}/${id}`);
+    return this.httpClient.delete(`${this.baseURL}/${id}`);
   }
 }
