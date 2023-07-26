18093e600809e34c771203debcb6cdbeb90f08e1
Youssef Ben Fraj <93080017+youssefbenfraj@users.noreply.github.com>
2023-07-26 14:04:28 +0100

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">=1.0"
+
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+subscription_id = "c7942088-7089-416c-97f1-d5c5d155a279"
+tenant_id       = "7917799a-9669-4e41-b87d-4c14fefd7c41"
+}
