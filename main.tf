055bd1d9ff1d69742c4191bf11107fc9aff0e4f4
Youssef Ben Fraj <93080017+youssefbenfraj@users.noreply.github.com>
2023-07-26 14:10:00 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 # Generate random resource group name
-resource "random_empp" "rg_name" {
+resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
 
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = random_empp.rg_name.id
+  name     = random_pet.rg_name.id
 }
 
-resource "random_empp" "azurerm_kubernetes_cluster_name" {
+resource "random_pet" "azurerm_kubernetes_cluster_name" {
   prefix = "cluster"
 }
 
-resource "random_empp" "azurerm_kubernetes_cluster_dns_prefix" {
+resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
   prefix = "dns"
 }
 
 resource "azurerm_kubernetes_cluster" "k8s" {
   location            = azurerm_resource_group.rg.location
-  name                = random_empp.azurerm_kubernetes_cluster_name.id
+  name                = random_pet.azurerm_kubernetes_cluster_name.id
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = random_empp.azurerm_kubernetes_cluster_dns_prefix.id
+  dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
 
   identity {
     type = "SystemAssigned"
