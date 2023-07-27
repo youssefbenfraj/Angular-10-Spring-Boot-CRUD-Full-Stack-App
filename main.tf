b4fa4a0bbeb35b303959f21a2401d0843e7faf1e
Youssef Ben Fraj <93080017+youssefbenfraj@users.noreply.github.com>
2023-07-27 09:46:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,5 +32,30 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
 
   identity {
     type = "SystemAssigned"
+    identity_ids = [var.managed_identity_id]
   }
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
+}
+
+output "cluster_username" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.username
+}
+
+output "cluster_password" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.password
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate
+}
+
+output "client_key" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key
+}
+
+output "cluster_ca_certificate" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate
 }
