4ec88ae2d257ddcb6c301e85d3d4c658cd11f85b
Youssef Ben Fraj <93080017+youssefbenfraj@users.noreply.github.com>
2023-07-27 14:17:30 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 
 variable "managed_identity_id" {
   type = string
-  default = "03861765-f705-46b4-b009-74a79e0f203a"
+  default = "aaae7bd3-001d-4e0d-b95f-1821a780b047"
 }
 
 resource "azurerm_resource_group" "aks_rg" {
@@ -40,30 +40,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     load_balancer_sku = "standard"
   }
 identity {
-    type = "SystemAssigned"
+   type        = "UserAssigned"
+    identity_ids = [var.managed_identity_id]
   }
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
-}
-
-output "cluster_username" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.username
-}
-
-output "cluster_password" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.password
-}
-
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate
-}
-
-output "client_key" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key
-}
-
-output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate
 }
