0c3077fdcfd0e6ae49c10722e097400e42d0ad6c
Youssef Ben Fraj <93080017+youssefbenfraj@users.noreply.github.com>
2023-07-27 09:29:02 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,12 +4,6 @@ variable "resource_group_location" {
   description = "Location of the resource group."
 }
 
-variable "resource_group_name_prefix" {
-  type        = string
-  default     = "Jenkins-Terraform-rg"
-  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
-}
-
 variable "node_count" {
   type        = number
   description = "The initial quantity of nodes for the node pool."
@@ -20,10 +14,4 @@ variable "msi_id" {
   type        = string
   description = "The Managed Service Identity ID. Set this value if you're running this example using Managed Identity as the authentication method."
   default     = "da1c0cdf-8f41-4998-acd8-0f3559ec47ff"
-}
-
-variable "username" {
-  type        = string
-  description = "The admin username for the new cluster."
-  default     = "azureadmin"
 }
