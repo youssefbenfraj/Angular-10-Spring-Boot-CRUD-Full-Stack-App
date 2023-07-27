87f150b8754eb176919d3de16e99bda815ad98cc
Youssef Ben Fraj <93080017+youssefbenfraj@users.noreply.github.com>
2023-07-27 09:34:38 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,45 +1,32 @@
-# Generate random resource group name
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
+provider "azurerm" {
+  features {}
+ subscription_id = "c7942088-7089-416c-97f1-d5c5d155a279"
+  use_msi        = true
 }
 
-resource "azurerm_resource_group" "Jenkins-Terraform-rg" {
-  location = var.resource_group_location
-  name     = random_pet.rg_name.id
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "Jenkins-Terraform-rg"
+  location = "East US"
 }
 
-resource "random_pet" "azurerm_kubernetes_cluster_name" {
-  prefix = "cluster"
-}
-
-resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
-  prefix = "dns"
-}
-
-resource "azurerm_kubernetes_cluster" "k8s-jenk-terra" {
-  location            = azurerm_resource_group.rg.location
-  name                = random_pet.azurerm_kubernetes_cluster_name.id
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
-
-  identity {
-    type = "SystemAssigned"
-  }
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "Terraform-cluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "Terraform-cluster-dns"
 
   default_node_pool {
     name       = "agentpool"
-    vm_size    = "Standard_D2_v2"
-    node_count = var.node_count
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
   }
-  linux_profile {
-    admin_username = var.username
 
-    ssh_key {
-      key_data = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
-    }
-  }
   network_profile {
     network_plugin    = "kubenet"
     load_balancer_sku = "standard"
+  }
+
+  identity {
+    type = "SystemAssigned"
   }
 }
