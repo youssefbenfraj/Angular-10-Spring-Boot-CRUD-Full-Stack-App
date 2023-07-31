d190dab2d688dab2b0a5f6918afba2c39a24553e
Youssef Ben Fraj <93080017+youssefbenfraj@users.noreply.github.com>
2023-07-31 11:54:06 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -55,3 +55,18 @@ output "cluster_ca_certificate" {
   value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate
   sensitive = true
 }
+
+output "client_key" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key
+    sensitive = true
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate
+    sensitive = true
+}
+
+output "host" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
+  sensitive = true
+}
